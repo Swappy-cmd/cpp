933d5179be54900c15089d6a87035f1206d3f26a
Swappy-cmd <swappy313@gmail.com>
2025-03-13 11:46:54 +0530

    q1

---
+++ A1q1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int side;
+    cout<<"enter side lenght of square:";
+    cin>>side;
+    cout<<"area of square is:"<<side*side;
+    return 0;
+}
