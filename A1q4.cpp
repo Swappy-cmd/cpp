383f57c6634e45d221acf0df61ece9ccc577070f
Swappy-cmd <swappy313@gmail.com>
2025-03-14 19:30:20 +0530

    q4

--- A1q4.cpp
+++ A1q4.cpp
@@ -1,4 +1,11 @@
 #include<iostream>
+using namespace std;
 int main(){
+    float radius;
+
+    cout<<"radius of circle:";
+    cin>>radius;
+
+    cout<<"circle radius is "<<(22/7)*radius*radius<<endl;
     return 0;
 }
