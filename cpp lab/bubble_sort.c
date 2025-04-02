70099d7cf29459055eebd8577296b8d725f49e47
Swappy-cmd <swappy313@gmail.com>
2025-04-02 10:50:33 +0530

    collage files add

---
+++ cpp lab/bubble_sort.c
@@ -0,0 +1,27 @@
+// BUBBLE SORT
+#include <stdio.h> 
+int main()
+{
+    int a[10], n, i, j, temp;
+    printf("\n Enter the size of n:");
+    scanf("%d", &n);
+    printf("\n Enter the array elements: \n");
+
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    for (i = 1; i < n; i++) /* ith smallest number bubbles up to its
+                                right spot in ith iteration */
+    {
+        for (j = 0; j < n - i; j++) /* bubbling starts from the "deepest numbers" and proceeds upwards */
+                                    /* element at jth position is "lighter" than the one on top, therefore jth element bubbles up */
+            if (a[j] >= a[j + 1])
+            {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+    }
+    printf("\n The sorted array is: \n");
+    for (i = 0; i < n; i++)
+        printf("%d\n", a[i]);
+}
---
+++ dsa/const var.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    const int num =25;
+    int num2 =25;
+    num2=35;
+    //num = 35; this line of code will show error -- variable is not modifiable
+
+    // we use const for 
+    return 0;
+}
