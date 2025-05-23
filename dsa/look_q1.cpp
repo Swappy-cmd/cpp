7a0a50c77c645824428b017bee384eba04451dcf
Swappy-cmd <swappy313@gmail.com>
2025-05-23 16:55:58 +0530

    new questions

---
+++ dsa/look_q1.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+//question 1
+
+// int main(){
+//     int i,num,fact=1;
+//     cout<<"enter number :";
+//     cin>>num;
+
+//     for (i=1;i<=num;i++){
+//         fact*=i;
+//     }
+//     cout<<"factorial is :"<<fact;
+//     return 0;
+// }
+
+//questions 2
+
+// int main(){
+//     int num,i;
+//     cout<<"enter number:";
+//     cin>>num;
+//     for (i=0;i<=num;i++){
+//         cout<<num<<"x"<<i<<"="<<num*i<<endl;
+//     }
+//     return 0;
+// }
+
+//question 3
+
+int main(){
+    int num,original,rev=0;
+    cout<<"enter number:";
+    cin>>num;
+    original=num;
+    while(num>0){
+        rev=(rev*10)+(num%10);
+        num=num/10;
+    }
+    cout<<rev<<endl;
+    if (rev==original){
+        cout<<"number is palindrome";
+    }else{
+        cout<<"number is NOT palindrome";
+    }
+    return 0;
+}
